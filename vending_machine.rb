3e1e1fc8b5130a2263c2e977dfd9abac32277ea7
Yoshiki Sekine <ysekine@klis.tsukuba.ac.jp>
2017-07-31 19:56:40 +0900

    STEP2 OK

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,31 +1,55 @@
 require 'set'
+require 'ostruct'
 
 class Machine
-  attr_accessor :total_putting
-  def initialize
-    @Usable_money = Set.new [10, 50, 100, 500, 1000].freeze # 名前は expected_money でもいいかもしれない
-    @total_putting = 0
+  attr_reader :remains, :stock
+
+  # クラスDrinkを作らずに，在庫データを保持する新しいStructを作る． 
+  Stock = Struct.new(:name, :price, :num)
+
+  def initialize(remains, usable_money)
+    # STEP 1
+    @remains, @Usable_money = remains, Set.new(usable_money) 
+
+    # STEP 2
+    @stock = Set.new
+    @stock << Stock.new("コーラ",120, 5)
+    @stock << Stock.new("レッドブル", 200, 4)
   end
 
-  def usable_money?(money)
+  
+  # STEP 1
+  def can?(money)
     @Usable_money.include?(money)
   end
 
   def put_money(money)
-    @total_putting += money 
+    @remains += money 
   end
   
   def refund
-    puts "refund #{@total_putting}"
-    @total_putting = 0
+    puts "refund #{@remains}"
+    @remains = 0
+  end
+  
+  # STEP 2
+  def add_stock(item, price, num)
+    @stock[item] += num
+  end
+  def reduce_stock(item, price, num)
+    @stock[item] -= num
+  end
+  
+  def ls
+    @stock.each{|item| p item.name}
   end
 end
 
 
-machine1 = Machine.new
+machine1 = Machine.new(0, [10, 50, 100, 500, 1000].freeze)
 
-p machine1.usable_money?(10)
-p machine1.usable_money?(1)
+p machine1.can?(10)
+p machine1.can?(1)
 p machine1.put_money(10)
 p machine1.put_money(10)
 p machine1.put_money(100)
@@ -33,5 +57,7 @@ p machine1.put_money(100)
 machine1.refund
 p machine1.put_money(100)
 p machine1.put_money(50)
+
+machine1.ls
 
 
