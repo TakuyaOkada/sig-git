679e1ad2303cc8dd20a0fa7500e19cb9c22bebfd
Yoshiki Sekine <ysekine@klis.tsukuba.ac.jp>
2017-08-04 18:56:38 +0900

    インスタンスメソッドの動作を整理した

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,5 +1,4 @@
 require 'set'
-require 'ostruct'
 
 class Machine
   attr_reader :remains, :stock
@@ -8,41 +7,56 @@ class Machine
   Stock = Struct.new(:name, :price, :num)
 
   def initialize(remains, usable_money)
-    # STEP 1
+    # STEP 1 投入金額の残金，扱えるお金
     @remains, @Usable_money = remains, Set.new(usable_money) 
 
     # STEP 2
+    # 在庫
     @stock = Set.new
     @stock << Stock.new("コーラ",120, 5)
     @stock << Stock.new("レッドブル", 200, 5)
     @stock << Stock.new("水", 100, 5)
 
     # STEP 3
+    # 売上
     @sales = 0
+    
+    # STEP 4
+    # 購入可能な商品を保持する集合
+    @available_list = Set.new
   end
 
   
   # STEP 1
+  # 使えるお金かどうか真偽
   def can?(money)
     @Usable_money.include?(money)
   end
 
+  # 自販機にお金を投入する
   def put_money(money)
     @remains += money 
   end
   
+  # 返金操作を行う
   def refund
     puts "refund #{@remains}"
     @remains = 0
   end
   
   # STEP 2
-  def available
-    available_list = []
-    @stock.each{|item| if item.price <= @remains && item.num > 0; available_list << item.name;end  }
-    return available_list
+  # 購入可能な商品の集合を「人間が読みやすい形に表現して(inspect)」返す
+  def available_list
+    @stock.each{|item| if item.price <= @remains && item.num > 0; @available_list << item.name;end  }
+    return @available_list.inspect
   end
 
+  # ある商品が購入可能か調べる
+  def available?(item)
+    return @available_list.include?(item)
+  end
+
+  # 商品を購入する
   def buy(want)
     @stock.each do |item| 
       if item.name == want
@@ -69,8 +83,10 @@ p machine1.put_money(50)
 
 p machine1.stock 
 
-puts "購入可能: #{machine1.available}"
-puts "おつり: #{machine1.buy("水")}"
+puts "購入可能: #{machine1.available_list}"
+p machine1.available?("水")
+machine1.buy("水")
+machine1.refund
 p machine1.stock
 
 
--- vending_machine_test.rb
+++ vending_machine_test.rb
@@ -1,22 +1,29 @@
 require './vending_machine.rb'
 
 # test
-puts "Test Start"
+puts "何かいりようかい"
 test_machine = Machine.new(0, [10, 50, 100, 500, 1000].freeze)
 #STEP0
 while true 
-  puts "input [coins]/refund/exit"
+  puts "input [coins]/buy/refund/exit"
   input = gets.chomp
   case input
   when "refund"
     test_machine.refund
   when "exit"
     break
+  when "buy"
+    puts "なににします？"  
+    want = gets.chomp
+    if test_machine.available?(want)
+      test_machine.buy(want)
+    end
   else
-    coin = input.to_i
-    if test_machine.can?(coin)# STEP2
+    coin = input.to_i # 期待されない文字列は0になる
+    if test_machine.can?(coin)# STEP 1
       test_machine.put_money(coin)
       puts "投入金額総計: #{test_machine.remains}"
+      puts "購入可能: #{test_machine.available_list}"
     else
       puts "釣銭: #{coin}"
     end
